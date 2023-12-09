b2e68b7b196c5ace4291441a0d674ea0652fe358
rxyhn <rxyhn.dev@gmail.com>
2023-12-10 01:24:11 +0700

    chore: update

--- app.py
+++ app.py
@@ -21,10 +21,29 @@ import mysql.connector
 
 class DatabaseHandler:
     def __init__(self, host, user, password, database):
-        self.connection = mysql.connector.connect(
-            host=host, user=user, passwd=password, database=database
-        )
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = mysql.connector.connect(
+                host=host, user=user, passwd=password
+            )
+            self.cursor = self.connection.cursor()
+
+            self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+            self.cursor.execute(f"USE {database}")
+            self.cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS book (
+                    ISBN VARCHAR(20) PRIMARY KEY,
+                    title TEXT NOT NULL,
+                    author VARCHAR(100) NOT NULL,
+                    year_published INT NOT NULL,
+                    price INT NOT NULL
+                )
+                """
+            )
+            self.connection.commit()
+
+        except mysql.connector.Error as err:
+            self.show_error_dialog(f"Database Error: {err}")
 
     def execute_query(self, query, params=None):
         try:
@@ -402,7 +421,7 @@ class BookManagementSystem(QMainWindow):
 
 def main():
     app = QApplication(sys.argv)
-    db_handler = DatabaseHandler("localhost", "root", "root", "BMS")
+    db_handler = DatabaseHandler("localhost", "root", "root", "LMS")
     main_window = BookManagementSystem(db_handler)
     main_window.show()
     sys.exit(app.exec_())
