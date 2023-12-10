42e1095a148c07be4b46d5de9e21aa323837d27e
rxyhn <rxyhn.dev@gmail.com>
2023-12-10 13:39:35 +0700

    chore: update

--- src/database.py
+++ src/database.py
@@ -9,20 +9,26 @@ class DatabaseHandler:
             )
             self.cursor = self.connection.cursor()
 
-            self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+            self.cursor.execute(f"SHOW DATABASES LIKE '{database}'")
+            if not self.cursor.fetchone():
+                self.cursor.execute(f"CREATE DATABASE {database}")
+
             self.cursor.execute(f"USE {database}")
-            self.cursor.execute(
-                """
-                CREATE TABLE IF NOT EXISTS book (
-                    ISBN VARCHAR(20) PRIMARY KEY,
-                    title TEXT NOT NULL,
-                    author VARCHAR(100) NOT NULL,
-                    year_published INT NOT NULL,
-                    price INT NOT NULL
+
+            self.cursor.execute("SHOW TABLES LIKE 'book'")
+            if not self.cursor.fetchone():
+                self.cursor.execute(
+                    """
+                    CREATE TABLE book (
+                        ISBN VARCHAR(20) PRIMARY KEY,
+                        title TEXT NOT NULL,
+                        author VARCHAR(100) NOT NULL,
+                        year_published INT NOT NULL,
+                        price INT NOT NULL
+                    )
+                    """
                 )
-                """
-            )
-            self.connection.commit()
+                self.connection.commit()
 
         except mysql.connector.Error as err:
             self.show_error_dialog(f"Database Error: {err}")
